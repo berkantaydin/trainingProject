82828dfe56c98af97052ac12e9e2f101fa4406c5
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-04-02 08:53:10 +0300

    -redis session

--- settings.py
+++ settings.py
@@ -161,7 +161,16 @@ EMAIL_FILE_PATH = '/tmp/emails-trainingApp'
 # For Debug-Toolbar
 INTERNAL_IPS = ('127.0.0.1',)
 
+# For using session in templates
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',)
+
+# For Redis Cache
+SESSION_ENGINE = 'redis_sessions.session'
+SESSION_REDIS_HOST = 'localhost'
+SESSION_REDIS_PORT = 6379
+SESSION_REDIS_DB = 0
+SESSION_REDIS_PASSWORD = ''
+SESSION_REDIS_PREFIX = 'trainingProject_trainingApp_'
